834c066e9e6c5cac4c1035699a62637c84f0917c
Marek Bartík <marek.bartik@bimproject.cz>
2014-11-12 15:44:10 -0600

    Board with pos feature

--- src/monopoly/Monopoly.java
+++ src/monopoly/Monopoly.java
@@ -15,8 +15,6 @@ public class Monopoly {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+  
     
 }
